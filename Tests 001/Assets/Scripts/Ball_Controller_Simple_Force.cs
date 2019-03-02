164591ab3efb1e41f2e8952091663d4dda713141
david <davidmakowski00@gmail.com>
2019-03-02 19:58:19 +0100

    Version 002 Initialised, Horizontal passing test with kin. equation from sebastian lague tutorials

---
+++ Tests 001/Assets/Scripts/Ball_Controller_Simple_Force.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controller_Simple_Force : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        if(Input.GetKeyDown("space")){
+            rb.AddForce(transform.forward * thrust, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Tests 001/Assets/Scripts/Ball_Controller_with_Target.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controller_with_Target : MonoBehaviour
+{   
+    public Rigidbody ball;
+    public Transform target;
+
+    public float h = 25;
+    public float gravity = -18;
+
+    public bool debugPath = true;
+    public bool launched = false;
+
+    List<Vector3> trajectory = new List<Vector3>();
+
+    void Start(){
+        ball.useGravity = false;
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            // get trajectory
+            trajectory = GetPath();
+
+            Launch();
+            launched = true;
+        }
+
+        if(debugPath && !launched){
+            DrawPath();
+        }else if(launched){
+            DrawPathFromList(trajectory);
+        }
+    }
+
+    void Launch(){
+        Physics.gravity = Vector3.up * gravity;
+        ball.useGravity = true;
+        ball.velocity = CalculateLaunchData().initialVelocity;
+    }
+
+    LaunchData CalculateLaunchData(){
+
+        float displacementY = target.position.y - ball.position.y;
+        Vector3 displacementXZ = new Vector3(target.position.x - ball.position.x, 0, target.position.z - ball.position.z);
+
+        float time = (Mathf.Sqrt(-2*h/gravity) + Mathf.Sqrt(2*(displacementY - h)/gravity));
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * h);
+        Vector3 velocityXZ = displacementXZ / time;
+
+        return new LaunchData(velocityXZ + velocityY, time);
+    }
+
+    void DrawPath(){
+        LaunchData launchData = CalculateLaunchData();
+        Vector3 previousDrawPoint = ball.position;
+
+        int resolution = 30;
+        for(int i = 1; i <= resolution; i++){
+            float simulationTime = i / (float)resolution * launchData.timeToTarget;
+            Vector3 displacement = launchData.initialVelocity * simulationTime + Vector3.up * gravity * simulationTime * simulationTime/2f;
+            Vector3 drawPoint = ball.position + displacement;
+            Debug.DrawLine(previousDrawPoint, drawPoint, Color.green);
+            previousDrawPoint = drawPoint;
+        }
+    }
+
+    List<Vector3> GetPath(){
+        /*
+          Here we want a way to draw the path without updating it while the ball is on it's way
+          So we just want to save the points and then when the ball is launched, we don't
+          want to update this any longer
+        */
+        LaunchData launchData = CalculateLaunchData();
+        Vector3 previousDrawPoint = ball.position;
+        List<Vector3> pathPoints = new List<Vector3>();
+        pathPoints.Add(previousDrawPoint);
+
+        int resolution = 30;
+        for(int i = 1; i <= resolution; i++){
+            float simulationTime = i / (float)resolution * launchData.timeToTarget;
+            Vector3 displacement = launchData.initialVelocity * simulationTime + Vector3.up * gravity * simulationTime * simulationTime/2f;
+            Vector3 drawPoint = ball.position + displacement;
+            pathPoints.Add(drawPoint);
+        }
+        return pathPoints;
+    }
+
+    void DrawPathFromList(List<Vector3> pathPoints)
+    {   
+        //get the start point
+        Vector3 previousDrawPoint = pathPoints[0];
+        for(int i=1; i < pathPoints.Count; i++)
+        {
+            Vector3 drawpoint = pathPoints[i];
+            Debug.DrawLine(previousDrawPoint, drawpoint, Color.green);
+            previousDrawPoint = drawpoint;
+        }
+    }
+
+    struct LaunchData{
+        public readonly Vector3 initialVelocity;
+        public readonly float timeToTarget;
+
+        public LaunchData(Vector3 initialVelocity, float timeToTarget)
+        {
+            this.initialVelocity = initialVelocity;
+            this.timeToTarget = timeToTarget;
+        }
+    }
+}
