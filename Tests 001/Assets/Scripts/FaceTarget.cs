d204a4ef3d70caefcc48b5ae24351fe4dba58561
david <davidmakowski00@gmail.com>
2019-03-03 16:39:10 +0100

    Receiver Movement added

---
+++ Tests 001/Assets/Scripts/FaceTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTarget : MonoBehaviour
+{   
+    /* This is probabaly only a temporary script to make the CB face
+       the receiver. Later this will probably move to some player-Controller
+     */
+
+    public Transform receiver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = receiver.position - transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = rotation;
+    }
+}
